102c8766908b4d4f8cc710efce93e8211446edef
dec9ue <decque@gmail.com>
2017-05-28 23:14:47 +0900

    working code (only types)

---
+++ circuit2blocks.hs
@@ -0,0 +1,14 @@
+
+data CircuitEdgeItem = C String | R String | L String | VS String | CS String
+		deriving Show
+data CircuitNodeItem = GND
+		deriving Show
+data CircuitTopoItem = Edge { name :: String, source :: String, dest :: String, edge_item :: CircuitEdgeItem}
+		| Node { name :: String, source:: String, node_item :: CircuitNodeItem}
+		 deriving Show
+type CircuitTopology = [CircuitTopoItem]
+
+data Coeff = Var String | One | Mul Coeff Coeff | Sum Coeff Coeff | Minus Coeff | Div Coeff Coeff
+
+
+
